44d9c0eeca3e17bb8fe63cd828d85e88d05c2ed9
Thanh Le <mihthanh27@gmail.com>
2022-06-05 21:03:28 +0700

    Remove click to hydrate

--- demo/pages/index.js
+++ demo/pages/index.js
@@ -5,11 +5,11 @@ import PageIntro from '../components/page-intro';
 // import Subscribe from '../components/subscribe';
 import lazyHydrate from 'next-lazy-hydrate';
 
-const Featured = lazyHydrate(() => import(/* webpackChunkName: "featured" */'../components/featured'), { on: ['click'] });
-const WhyUs = lazyHydrate(() => import(/* webpackChunkName: "whyus" */'../components/whyus'), { on: ['click'] });
-const ProductsFeatured = lazyHydrate(() => import(/* webpackChunkName: "product-featured" */'../components/products-featured'), { on: ['click'] });
-const Footer = lazyHydrate(() => import(/* webpackChunkName: "footer" */'../components/footer'), { on: ['click'] });
-const Subscribe = lazyHydrate(() => import(/* webpackChunkName: "subscribe" */'../components/subscribe'), { on: ['click'] });
+const Featured = lazyHydrate(() => import(/* webpackChunkName: "featured" */'../components/featured'));
+const WhyUs = lazyHydrate(() => import(/* webpackChunkName: "whyus" */'../components/whyus'));
+const ProductsFeatured = lazyHydrate(() => import(/* webpackChunkName: "product-featured" */'../components/products-featured'));
+const Footer = lazyHydrate(() => import(/* webpackChunkName: "footer" */'../components/footer'));
+const Subscribe = lazyHydrate(() => import(/* webpackChunkName: "subscribe" */'../components/subscribe'));
 
 const IndexPage = () => {
   return (
