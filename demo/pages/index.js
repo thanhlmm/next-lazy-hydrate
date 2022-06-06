a8b6fe4a5814ce0eeb33c9054b724f45297f1241
Thanh Le <mihthanh27@gmail.com>
2022-06-06 02:37:24 +0000

    docs: static component for demo

--- demo/pages/index.js
+++ demo/pages/index.js
@@ -6,11 +6,22 @@ import Featured from '../components/featured';
 // import Subscribe from '../components/subscribe';
 import lazyHydrate from 'next-lazy-hydrate';
 
-const WhyUs = lazyHydrate(() => import(/* webpackChunkName: "whyus" */'../components/whyus'));
-const ProductsFeatured = lazyHydrate(() => import(/* webpackChunkName: "product-featured" */'../components/products-featured'));
-const Subscribe = lazyHydrate(() => import(/* webpackChunkName: "subscribe" */'../components/subscribe'));
-const Animation = lazyHydrate(() => import(/* webpackChunkName: "animation" */'../components/animation'));
-const Footer = lazyHydrate(() => import(/* webpackChunkName: "footer" */'../components/footer'));
+const WhyUs = lazyHydrate(() => import(/* webpackChunkName: "whyus" */'../components/whyus')); // Static component
+const ProductsFeatured = lazyHydrate(
+  () =>
+    import(
+      /* webpackChunkName: "product-featured" */ '../components/products-featured'
+    ),
+  { on: [['visible', () => ({ rootMargin: '50px' })]] }
+); // Interactive component
+const Subscribe = lazyHydrate(() =>
+  import(/* webpackChunkName: "subscribe" */ '../components/subscribe')
+);  // Static component
+const Animation = lazyHydrate(
+  () => import(/* webpackChunkName: "animation" */ '../components/animation'),
+  { on: [['visible', () => ({ rootMargin: '50px' })]] }
+); // Interactive component
+const Footer = lazyHydrate(() => import(/* webpackChunkName: "footer" */'../components/footer'));  // Static component
 
 const IndexPage = () => {
   return (
